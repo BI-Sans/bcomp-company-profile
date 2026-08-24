921ebf48b2444418bbf0b11e7ea794e7325cab83
BI-Sans <ilhambagus477@gmail.com>
2026-08-25 00:12:49 +0700

    Fix HTTPS mixed content on vercel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,6 +13,8 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        //
+        if  ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
